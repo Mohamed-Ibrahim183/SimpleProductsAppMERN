a1e7367022bd100c792e0da430a4be0850856967
Mohamed-Ibrahim183 <mi062921@gmail.com>
2025-02-16 19:58:15 +0200

    `Update package.json and server.js for index.js usage`

--- backend/index.js
+++ backend/index.js
@@ -11,24 +11,23 @@ const app = express();
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 // Enable CORS for all origins
-app.use(cors({ origin: "*" }));
+app.use(cors());
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
+});
 
 const PORT = process.env.PORT || 5000;
-const __dirname = path.dirname(path.resolve());
+// const __dirname = path.dirname(path.resolve());
 
 // routes
 app.use("/api/products", productRoutes);
 app.get("/hello", (req, res) => res.send("Hello World"));
 app.get("/", (req, res) => res.send("Hello World"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
-
-app.listen(PORT, () => {
-  connectDB();
-  console.log("Server started at http://localhost:" + PORT);
-});
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
